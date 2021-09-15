1de6d8e45b79ed8f845ce7685856607159c43e7a
Orbt <gabrielb@adekz.com>
2021-09-15 19:51:21 -0300

    Entrar

--- pages/entrar/index.tsx
+++ pages/entrar/index.tsx
@@ -3,22 +3,31 @@ import Image from 'next/image'
 import { motion } from 'framer-motion'
 
 import Button from '../../components/Button'
+import style from '../entrar/style.module.css'
 
 const Promocoes = () => {
 	return (
-		<motion.div
-			exit={{ opacity: 0 }}
-			initial={{ opacity: 0, y: 50 }}
-			animate={{ opacity: 1, y: 0 }}
-		>
-			<Image
-				src="https://api.orbt.com.br/barbosa/logo.svg"
-				alt="Picture of the author"
-				width={500}
-				height={500}
-			/>
-			<Button></Button>
-		</motion.div>
+		<div className={style.container}>
+			<motion.div
+				exit={{ opacity: 0 }}
+				initial={{ opacity: 0, y: 100 }}
+				animate={{ opacity: 1, y: 0 }}
+			>
+				<Image
+					src="https://api.orbt.com.br/barbosa/logo.png"
+					alt="Picture of the author"
+					width={500}
+					height={200}
+				/>
+			</motion.div>
+			<motion.div
+				exit={{ opacity: 0 }}
+				initial={{ opacity: 0, y: 50 }}
+				animate={{ opacity: 1, y: 0 }}
+			>
+				<Button></Button>
+			</motion.div>
+		</div>
 	)
 }
 
